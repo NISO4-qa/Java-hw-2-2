a164b37706b4c3e7a5d7795bbc8b7f1829cab0ff
Nikita Chalyi <nikita4556@gmail.com>
2024-02-28 23:16:14 +0300

    bonus programme

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int onphone = (100);
+
+        int amountadded =1400;
+
+        int bonus = amountadded / 100;
+int bonusless1000 =0;
+        int overall2 = onphone + amountadded;
+            if (amountadded < 1000)  System.out.println("Сумма на телефоне:"+ " "  + overall2 + " " + "Бонусов нет");
+            else if (amountadded> 1000) {
+                int overall = onphone + amountadded + bonus;
+                System.out.println("Итоговые бонусы:" + bonus );
+                System.out.println("Сумма на телефоне:" + overall );
+            }
+
+
+
+
+
+
+
+    // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
     }
 }
