a6b9fa3c50e62b505e5a21f2323047fb20b7ab01
Nikita Chalyi <nikita4556@gmail.com>
2024-02-29 15:17:19 +0300

    syntax edit

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int onphone = (100);
+        int onPhone = (100);
 
-        int amountadded =1400;
+        int amountAdded = 200;
 
-        int bonus = amountadded / 100;
-int bonusless1000 =0;
-        int overall2 = onphone + amountadded;
-            if (amountadded < 1000)  System.out.println("Сумма на телефоне:"+ " "  + overall2 + " " + "Бонусов нет");
-            else if (amountadded> 1000) {
-                int overall = onphone + amountadded + bonus;
-                System.out.println("Итоговые бонусы:" + bonus );
-                System.out.println("Сумма на телефоне:" + overall );
-            }
+        int bonus = amountAdded / 100;
+        int overall2 = onPhone + amountAdded;
+        if (amountAdded < 1000) System.out.println("Сумма на телефоне:" + " " + overall2 + " " + "Бонусов нет");
+        else if (amountAdded > 1000) {
+            int overall = onPhone + amountAdded + bonus;
+            System.out.println("Итоговые бонусы:" + bonus);
+            System.out.println("Сумма на телефоне:" + overall);
+        }
 
-
-
-
-
-
-
-    // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
